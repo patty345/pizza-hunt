06c5e938996752d4787a3c73a69781b9bb2b8a2d
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-03-20 20:07:28 -0500

    added the data validation

--- models/Comment.js
+++ models/Comment.js
@@ -1,58 +1,63 @@
 const { Schema, model, Types } = require("mongoose");
-const dateFormat = require('../utils/dateFormat');
+const dateFormat = require("../utils/dateFormat");
 
 const ReplySchema = new Schema(
   {
     // set custom id to avoid confusion with parent comment's _id field
     replyId: {
       type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId()
+      default: () => new Types.ObjectId(),
     },
     replyBody: {
-      type: String
+      type: String,
+      required: true,
+      trim: true
     },
     writtenBy: {
-      type: String
+      type: String,
+      required: true,
     },
     createdAt: {
       type: Date,
       default: Date.now,
-      get: createdAtVal => dateFormat(createdAtVal)
-    }
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
   },
   {
     toJSON: {
-      getters: true
-    }
+      getters: true,
+    },
   }
 );
 
 const CommentSchema = new Schema(
   {
     writtenBy: {
-      type: String
+      type: String,
+      required: true,
     },
     commentBody: {
-      type: String
+      type: String,
+      required: true,
     },
     createdAt: {
       type: Date,
       default: Date.now,
-      get: createdAtVal => dateFormat(createdAtVal)
+      get: (createdAtVal) => dateFormat(createdAtVal),
     },
     // use ReplySchema to validate data for a reply
-    replies: [ReplySchema]
+    replies: [ReplySchema],
   },
   {
     toJSON: {
       virtuals: true,
-      getters: true
+      getters: true,
     },
-    id: false
+    id: false,
   }
 );
 
-CommentSchema.virtual('replyCount').get(function() {
+CommentSchema.virtual("replyCount").get(function () {
   return this.replies.length;
 });
 
